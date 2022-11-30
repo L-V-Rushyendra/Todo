6401c13c2c4ccd9721f6442fb5d3778126b018d5
L Venkata Rushyendra <98335152+L-V-Rushyendra@users.noreply.github.com>
2022-11-30 09:58:00 +0530

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import "./styles.css";
+
+export default function App() {
+  return (
+    <body>
+      <div className="App">
+        <h1>Hello CodeSandbox</h1>
+        <h1 className="title">You can do it!</h1>
+        <div className="combine">
+          <input type="text" />
+          <button className="formsubmit">Got this</button>
+        </div>
+      </div>
+    </body>
+  );
+}
